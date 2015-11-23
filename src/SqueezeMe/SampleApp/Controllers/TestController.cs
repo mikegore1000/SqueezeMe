247c097ba73ae697c45d4a1c78effeaf23120f42
Mike Gore <mikegore1000@msn.com>
2015-11-23 18:51:32 +0000

    Initial commit

---
+++ src/SqueezeMe/SampleApp/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Http;
+
+namespace SampleApp.Controllers
+{
+    public class TestController : ApiController
+    {
+        [Route("test/{generateTo}")]
+        public Response Get(int generateTo)
+        {
+            var response = new Response { Values = new List<int>()};
+
+            for (int i = 0; i < generateTo; i++)
+            {
+                response.Values.Add(i);
+            }
+
+            return response;
+        }
+    }
+
+    public class Response
+    {
+        public List<int> Values { get; set; }
+    }
+}
---
+++ src/SqueezeMe/SqueezeMe/ICompressor.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SqueezeMe
+{
+    public interface ICompressor
+    {
+        string ContentEncoding { get; }
+
+        Task CompressAsync(HttpContent source, Stream destination);
+    }
+}
