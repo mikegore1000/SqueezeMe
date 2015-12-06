0cbf30211e98a87893fd14a9c31c303e08516f9c
Mike Gore <mikegore1000@msn.com>
2015-12-06 21:21:56 +0000

    Tidied up code.

--- src/SqueezeMe/SqueezeMe/CompressionMiddleware.cs
+++ src/SqueezeMe/SqueezeMe/CompressionMiddleware.cs
@@ -1,18 +1,17 @@
-﻿using System;
-using System.Linq;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Microsoft.Owin;
-
-namespace SqueezeMe
+﻿namespace SqueezeMe
 {
-    using System.Diagnostics;
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Microsoft.Owin;
     using System.IO;
-    using AppFunc = Func<IDictionary<string, object>, Task>;
+    using System.Net.Http.Headers;
 
     public class CompressionMiddleware
     {
-        private AppFunc next;
+        private const int BufferSize = 1024 * 4;
+        private Func<IDictionary<string, object>, Task> next;
 
         private readonly List<ICompressor> compressors = new List<ICompressor>()
         {
@@ -20,7 +19,7 @@ namespace SqueezeMe
             new DeflateCompressor()
         };
 
-        public CompressionMiddleware(AppFunc next)
+        public CompressionMiddleware(Func<IDictionary<string, object>, Task> next)
         {
             this.next = next;
         }
@@ -28,8 +27,7 @@ namespace SqueezeMe
         public async Task Invoke(IDictionary<string, object> environment)
         {
             var context = new OwinContext(environment);
-            var destinationStream = context.Response.Body;
-            long contentLength = 0;
+            var httpOutputStream = context.Response.Body;
 
             using (var responseStream = new MemoryStream())
             {
@@ -40,41 +38,38 @@ namespace SqueezeMe
 
                 if (compressor != null && context.Response.Body != null)
                 {
-                    responseStream.Position = 0;
-                    contentLength = responseStream.Length;
-
-                    // COMPRESS!!!
-                    using (var compressedStream = new MemoryStream())
-                    {
-                        using (var compressionStream = compressor.CreateStream(compressedStream))
-                        {
-                            await responseStream.CopyToAsync(compressionStream, 1024 * 4);
-                        }
-
-                        contentLength = compressedStream.Length;
-                        compressedStream.Position = 0;
-                        compressedStream.CopyTo(destinationStream);
-                    }
+                    await Compress(context, httpOutputStream, responseStream, compressor);
                 }
-
-                context.Response.Headers["Content-Encoding"] = compressor.ContentEncoding;
             }
 
-            context.Response.Body = destinationStream;
+            context.Response.Body = httpOutputStream;
+        }
+
+        private static async Task Compress(OwinContext context, Stream httpOutputStream, MemoryStream responseStream, ICompressor compressor)
+        {
+            responseStream.Position = 0;
+            long contentLength = 0;
+
+            using (var compressedStream = new MemoryStream())
+            {
+                using (var compressionStream = compressor.CreateStream(compressedStream))
+                {
+                    await responseStream.CopyToAsync(compressionStream, BufferSize);
+                }
+
+                contentLength = compressedStream.Length;
+                compressedStream.Position = 0;
+                compressedStream.CopyTo(httpOutputStream);
+            }
+
+            context.Response.Headers["Content-Encoding"] = compressor.ContentEncoding;
             context.Response.ContentLength = contentLength;
         }
 
         private ICompressor GetCompressor(IOwinRequest request)
         {
             return (from c in compressors
-                    from e in request.Headers.GetCommaSeparatedValues("Accept-Encoding").Select(x => {
-                        // TODO: Need to tidy this up
-                        var valueAndQuality = x.Split(';');
-                        var value = valueAndQuality.Length > 0 ? valueAndQuality[0] : null;
-                        var quality = valueAndQuality.Length > 1 ? float.Parse(valueAndQuality[1].Substring(2)) : 1;
-
-                        return new { Value = value, Quality = quality };
-                    })
+                    from e in request.Headers.GetCommaSeparatedValues("Accept-Encoding").Select(x => new StringWithQualityHeaderValue(x))
                     orderby e.Quality descending
                     where string.Compare(c.ContentEncoding, e.Value, StringComparison.InvariantCultureIgnoreCase) == 0
                     select c).FirstOrDefault();
