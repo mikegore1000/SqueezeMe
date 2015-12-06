79161a48029e56d1dd0c104e1cb92e007a62de45
Mike Gore <mikegore1000@msn.com>
2015-12-06 22:33:11 +0000

    Ported tests to use the OWIN middleware.

---
+++ src/SqueezeMe/SqueezeMe.UnitTests/Controllers/FakeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace SqueezeMe.UnitTests.Controllers
+{
+    public class FakeController : ApiController
+    {
+        [Route("test")]
+        public string Get()
+        {
+            return "Test";
+        }
+    }
+}
